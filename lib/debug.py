5b7841b16ad59269553032fb7c71f678bab46208
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-27 09:30:39 +0300

    completion

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from anagram import Anagram
+# from anagram import Anagram
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
